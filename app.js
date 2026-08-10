eb98eb21a473561d3f10ec511a8f0cf7422a6830
saurabh <saurabhpandey0040@gmail.com>
2026-08-10 11:11:12 +0530

    changes

--- app.js
+++ app.js
@@ -168,6 +168,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error.ejs", { err });
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
